070c4e9925ccd00a38d126d8d589f137e56dd300
Milan Rawat <rawatbleachrock@gmail.com>
2019-02-09 15:31:34 +0530

    commit

--- app.js
+++ app.js
@@ -4,26 +4,23 @@ const app = express();
 const multer = require('multer');
 const path  = require('path');
 const port = process.env.port||1234;
-const upload = require('./api/multer/uploadCode');
+const routes = require('./api/routes/empRoutes');
+const adminRoutes = require('./api/routes/adminRoutes');
 
-const storage = multer.diskStorage({
-    destination:'./uploads/',
-    filename:function(req,file,cb){
-        cb(null,file.fieldname + path.extname(file.originalname));
 
-    }
-})
-const uploadDetails = upload(storage);
+
 
 
 
 app.use(cors());
 app.use(express.static("public"));
 
+
 const bodyparser = require("body-parser");
 app.use(bodyparser.urlencoded({extended:false}));
 app.use(bodyparser.json());
-var routes = require('./api/routes/empRoutes');
-routes(app);
+
+app.use('/', routes);
+app.use('/admin',adminRoutes);
 
 app.listen(port);
